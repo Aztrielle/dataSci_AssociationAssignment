5b04efb1910eae513c3d07aab99a51ba9362ed7c
Karlo Rafael G. Dela Pena <2222630@slu.edu.ph>
2026-03-06 15:23:04 +0800

    src/centralTendency.py: added script to count how many times each binary column has 1

---
+++ src/centralTendency.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Load dataset
+file_path = "Transformed_TouristDestinations.csv"
+df = pd.read_csv(file_path)
+
+print("\nCount of '1' values per column\n")
+
+for col in df.columns:
+    
+    # Count how many times 1 appears in the column
+    count_ones = (df[col] == 1).sum()
+    print(f"{col}: {count_ones}")
